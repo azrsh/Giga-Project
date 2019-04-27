6edc394316c7ccb1abc0ee7ffbce75455bc3ca64
azarashi2931 <kousuke.urakawa@gmail.com>
2019-04-28 05:58:07 +0900

    音楽再生用ライブラリを追加

---
+++ Giga_v1/TonePlayer.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "TonePlayer.hpp"
+
+void TonePlayer::play()
+{
+    for (int thisNote = 0; thisNote < size; thisNote++)
+    {
+        // to calculate the note duration, take one second divided by the note type.
+        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(2, melody[thisNote], noteDuration);
+
+        // to distinguish the notes, set a minimum time between them.
+        // the note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        // stop the tone playing:
+        noTone(2);
+    }
+}
