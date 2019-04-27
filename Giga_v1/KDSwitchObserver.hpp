1cc3d7f2f28c771cd93ed909b69e62dafae560e2
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2019-04-28 05:58:58 +0900

    音楽再生とラインの自動調整コードを追加

--- Giga_v1/KDSwitchObserver.hpp
+++ Giga_v1/KDSwitchObserver.hpp
@@ -1,9 +1,13 @@
 #ifndef KD_SWITCH_OBSERVER_h
 #define KD_SWITCH_OBSERVER_h
 
+#include "TonePlayer.hpp"
+#include "Pitches.hpp"
+
 class KDSwitchObserver
 {
   public:
+    KDSwitchObserver(KDLineSensors *lineSensors) : lineSensorsInstance(lineSensors){};
     void reset();
     bool readMainSwitch();
     bool readDashSwitch();
@@ -14,6 +18,10 @@ class KDSwitchObserver
     bool previousDashSwitch = false;
     bool mainSwitch = false;
     bool dashSwitch = false;
+    KDLineSensors *lineSensorsInstance;
+    int melody[1] = {Pitches::NoteC4};
+    int noteDurations[1] = {4};
+    TonePlayer tonePlayer = TonePlayer(1, melody, noteDurations);
 };
 
 #endif
