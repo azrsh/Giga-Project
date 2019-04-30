35ab73d4dd41a9065172d04d5608c904de522c5e
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2019-04-30 17:35:11 +0900

    改行などの追加

--- Giga_v1/KDInterThreadData.cpp
+++ Giga_v1/KDInterThreadData.cpp
@@ -2,29 +2,30 @@
 #include "KDInterThreadData.hpp"
 #include "KDSharedObjects.hpp"
 
-void KDInterThreadData::read(VectorXY_t *vector ,unsigned long *timeStamp)
+void KDInterThreadData::read(VectorXY_t *vector, unsigned long *timeStamp)
 {
-   if(isHold) 
-   {
-       *vector = pastVector;
-       *timeStamp = pastTimeStamp;
-       return;
-   }
+    if (isHold)
+    {
+        *vector = pastVector;
+        *timeStamp = pastTimeStamp;
+        return;
+    }
 
-   isHold = true;
-    *vector = {(int)this->x,(int)this->y};
+    isHold = true;
+    *vector = {(int)this->x, (int)this->y};
     *timeStamp = this->timeStamp;
-    pastVector = {(int)this->x,(int)this->y};
+    pastVector = {(int)this->x, (int)this->y};
     pastTimeStamp = this->timeStamp;
     isHold = false;
 }
 
 void KDInterThreadData::push(VectorXY_t vector)
 {
-   if(isHold) return;
-   isHold = true;
-   this->x = vector.x;
-   this->y = vector.y;
-   this->timeStamp = millis();
-   isHold = false;
+    if (isHold)
+        return;
+    isHold = true;
+    this->x = vector.x;
+    this->y = vector.y;
+    this->timeStamp = millis();
+    isHold = false;
 }
--- Giga_v1/KDInterThreadData.hpp
+++ Giga_v1/KDInterThreadData.hpp
@@ -5,14 +5,15 @@
 class KDInterThreadData
 {
   public:
-    void read(VectorXY_t *vector ,unsigned long *timeStamp);
+    void read(VectorXY_t *vector, unsigned long *timeStamp);
     void push(VectorXY_t vector);
+
   private:
     volatile bool isHold = false;
     volatile char x = 0;
     volatile char y = 0;
     volatile unsigned long timeStamp = 0;
-    VectorXY_t pastVector = {0,0};
+    VectorXY_t pastVector = {0, 0};
     unsigned long pastTimeStamp = 0;
 };
 
